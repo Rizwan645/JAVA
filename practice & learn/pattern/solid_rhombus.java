eb6ecc38de78fdb215c7eb0173f7d96def77c0df
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-03-14 11:35:25 +0530

    Create solid_rhombus.java

---
+++ practice & learn/pattern/solid_rhombus.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    System.out.print("Enter the n : ");
+	    int n = sc.nextInt ();
+	    for (int i=1;i<=n;i++){
+	        for (int j=n;j>i ;j-- ) {
+	            System.out.print(" ");
+	        }
+	        for (int j=1;j<=n ;j++ ) {
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	        
+	    }
+    }
+}
